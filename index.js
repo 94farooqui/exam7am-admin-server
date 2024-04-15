52fc50f3ea19602b21050bae1bc6d8fa267b8a79
94farooqui <94.farooqui@gmail.com>
2024-04-15 18:32:50 +0400

    added PORT env

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ import cors from 'cors'
 
 dotenv.config()
 const app = express()
+const port = process.env.PORT || 2000
 app.use(express.json())
 app.use(cors())
 
 connectDB()
 
 app.use('/api', router)
-app.listen(2000, () => {
-    console.log("Server is running on port 2000")
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 })
