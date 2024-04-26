bf9645619cb2716b56aaed4df9f483235b7d96e1
94farooqui <94.farooqui@gmail.com>
2024-04-26 22:39:32 +0400

    added route for new question

--- routes/admin/modules.route.js
+++ routes/admin/modules.route.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { getAllModules,addNewModule,getModuleCategoires,addModuleCategory } from '../../controller/modules.controller.js';
+import { getAllModules,addNewModule,getModuleCategoires,addModuleCategory,getModuleCategoryDetails,addNewQuestion } from '../../controller/modules.controller.js';
 
 const moduleRouter = express.Router()
 
 moduleRouter.get('/',getAllModules)
 moduleRouter.post('/',addNewModule)
 moduleRouter.get('/:module/categories',getModuleCategoires)
+moduleRouter.get('/:module/categories/:category',getModuleCategoryDetails)
+moduleRouter.post('/:module/categories/:category/newQuestion',addNewQuestion)
 moduleRouter.post('/:module/categories',addModuleCategory)
 
 
